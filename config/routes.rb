8a4cdfc62ac9f2b83fa9e88821e0a934487af128
Bipin Manandhar <kubipin07@gmail.com>
2018-10-22 07:36:48 +0545

    Add static dashboard page and group model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'dashboard#index'
+
   devise_for :users
+  resources :groups
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
